7ec4553f6f08d82de22d699c578ca24324aa21db
Tess Huang <tesshuangxj@yahoo.com>
2019-10-22 15:02:55 -0700

    Wire data using Redux

--- src/components/TaskList.test.js
+++ src/components/TaskList.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TaskList from './TaskList';
+import { PureTaskList } from './TaskList';
 import { withPinnedTasks } from './TaskList.stories';
 
 it('renders pinned task at the start of the list', () => {
   const div = document.createElement('div');
   const events = { onPinTask: jest.fn(), onArchiveTask: jest.fn() };
-  ReactDOM.render(<TaskList tasks= {withPinnedTasks} {...events} />, div);
+  ReactDOM.render(<PureTaskList tasks= {withPinnedTasks} {...events} />, div);
 
   // expect the task title "Task 6" to be render first 
   const lastTaskInput = div.querySelector('.list-item:nth-child(1) input[value="Task 6 (pinned)"]');
