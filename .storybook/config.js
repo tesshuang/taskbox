f3a36a9d089ae37b9986447525862e49b1e67273
Tess Huang <tesshuangxj@yahoo.com>
2019-10-23 10:46:29 -0700

    Add chromatic to the project

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,10 @@
 import { configure } from '@storybook/react';
 import requireContext from 'require-context.macro';
+import 'storybook-chromatic';
 
 import '../src/index.css';
 
-const req = requireContext('../src', true, /\.stories\.js$/);
+const req = requireContext('../src/components', true, /\.stories\.js$/);
 
 function loadStories(){
   req.keys().forEach(filename => req(filename));
